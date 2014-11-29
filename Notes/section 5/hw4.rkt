34e1c4a81dd2fd35320bd69ff3b7d0e5425d1311
ee7klt <kltah1@gmail.com>
2014-11-29 13:44:09 +0800

    hw4

--- Notes/section 5/hw4.rkt
+++ Notes/section 5/hw4.rkt
@@ -2,7 +2,7 @@
 
 
 (provide (all-defined-out))
-
+(require rackunit)
 
 
 
@@ -15,8 +15,33 @@
 
 
 (define (sequence low high stride)
-  (let (ans null)
-  (if (= high 0)
-      ans
-      (
+      (if (> low high)
+          null
+          (cons low (sequence (+ low stride) high stride))))
+
+
+(define (string-append-map xs suffix)
+   (let ([f (lambda (x) (string-append x suffix))])
+     (map f xs)))
+  
+(define addone
+  (lambda (x)
+    (+ x 1)))
+
+(define a '(1 2 3))
+
+(define b '("say" "buy" "clock"))
+
+(define f
+  (lambda (x)
+    (string-append x "ing")))
+
+(string-append-map b "ing")
+
+
+
+(define (list-nth-mod xs n)
+  cond [(< n 0) (error "list-nth-mod: negative number")]
+       [(null? xs) (error "list-nth-mod: empty list")]
+  
   
